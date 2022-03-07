55e1f3a7a1f18881901d0d18b3e08a9700d71343
HOME-PC\DIMITRIS PERTSINIDIS <prtmediaco@gmail.com>
2022-03-07 02:03:49 +0200

    Created the Home Page for the Blog with styling

---
+++ resources/views/layouts/posts-grid.blade.php
@@ -0,0 +1,15 @@
+
+@include('layouts.post-featured-card', ['post' => $posts[0]])
+
+@if($posts->count() > 1)
+    <div class="lg:grid lg:grid-cols-6">
+
+        @foreach($posts->skip(1) as $post)
+            @include('layouts.post-card', [
+                'post' => $post, 
+                'changeCol'=> $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' 
+            ])
+        @endforeach
+
+    </div>
+@endif
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,25 +1,17 @@
-@extends('layout')
+@extends('layouts.layout')
 
 @section('content')
 
-    @foreach ($posts as $post)
-    <article>
 
-        <h1>
-            <a href="/posts/{{ $post->slug }}">
-                {{ $post->title }}
-            </a>
-        </h1>
-        
-        <p>
-            By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-        </p>
+@include('layouts._posts-header')
 
-        <div>
-            {{ $post->excerpt }}
-        </div>
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
 
-    </article>
-    @endforeach
+    @if($posts->count())
+        @include('layouts.posts-grid', ['post' => $posts])
+    @else
+        <p class="text-center">No posts yet. Please check later.</p>
+    @endif
+</main>
 
 @endsection
