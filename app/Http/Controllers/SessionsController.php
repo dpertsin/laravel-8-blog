e529c1d5cb850eb464534057dd3802079be3ee4e
HOME-PC\DIMITRIS PERTSINIDIS <prtmediaco@gmail.com>
2022-03-10 14:10:40 +0200

    Forms and Authentication

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Validation\ValidationException;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        // validate the request
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        // attempt to authenticate and log in the use
+        //based on the provided credentials
+        if (auth()->attempt($attributes)) {
+            // redirect with a success flash message
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        // auth failed #1
+        // return back()
+        //     ->withInput()
+        //     ->withErrors(['email' => 'Your provided credentials could not be verified.']);
+
+        // auth failed #2
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified.'
+        ]);
+    }
+
+    public function destroy()
+    {
+        auth()->logout();
+
+        return redirect('/')->with('success', 'Goodbye!');
+    }
+}
